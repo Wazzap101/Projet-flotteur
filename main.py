ed3e44463aacb4d3010f38f0aa857d9117380cd4
GHITATT <ghita.tazeroualt@ensta-bretagne.org>
2024-05-24 14:32:43 +0200

    sdfghjk

--- main.py
+++ main.py
@@ -34,41 +34,39 @@ def tracer_rho():
     plt.legend()
     plt.show()
 
-#tracer_rho()
+def f1(depth):
+    if depth < min(profondeur) or depth > max(profondeur):
+        if depth < min(profondeur):
+            depth = min(profondeur)
+        else:
+            depth = max(profondeur)
+    return f(depth)
 
-mf = 20 #en kg
-V = 19.8e-3 #en m3
+
+mf = 40 #en kg
+V = mf/f(max(profondeur)) #en m3
 D = 11.3e-2 #en m
 S = np.pi*(D/2)**2 #en m3
-C =1
-N=1000
+C = 1
+N=5000
 h = 0.05
-t = np.linspace(0, 800, N+1)
+t = np.linspace(0, 5000, N+1)
 g = 9.8 #m/s-2
 
 
 def g(X):
-    depth = -X[0]
-    if depth.any() < min(profondeur) or depth.any() > max(profondeur):
-        # Gestion des cas où la profondeur est en dehors de la plage des données
-        # Vous pouvez ajuster cela selon vos besoins
-        if depth.any() < min(profondeur):
-            depth = min(profondeur)
-        else:
-            depth = max(profondeur)
-        rho_interp = f(depth)
-    else:
-        rho_interp = f(depth)
-    ma = 0.5#rho_interp*4*np.pi*(D/2)**3 /3
+    depth = X[0]
+    rho_interp = f1(depth)
+    ma = rho_interp*4*np.pi*(D/2)**3 /3
     m = ma+mf
-    return np.array([X[1], (-rho_interp*V/m + 1)*9.8 - rho_interp*S*C*X[1]*abs(X[1])/(2*m)])
+    return np.array([X[1], (-rho_interp*V/m + mf/m)*9.8- rho_interp*S*C*X[1]*abs(X[1])/(2*m)])
 
 
 y = np.zeros(N+1)
 X = np.zeros((N+1, 2))
 v = np.zeros(N+1)
 
-X[0] = [x[0]+0.5, 0]
+X[0] = [x[0], 0]
 y[0] = X[0][0]
 v[0] = X[0][1]
 def euler():
@@ -76,8 +74,7 @@ def euler():
         X[i+1] = X[i] + h*g(X[i])
         y[i+1] = X[i+1][0]
         v[i+1] = X[i+1][1]
-    print(v[-1])
-    plt.plot(t, np.array(y), '+', label='Profondeur')
+    plt.plot(t, np.array(y), '--', label='Profondeur')
     plt.plot(t, v, label='vitesse')
     plt.xlabel("temps")
     plt.title("z = f(t)")
